f897da3ec4dc3abad5167a61e9d919ea78ee0abe
analuisavier <analuivieira@hotmail.com>
2025-05-29 23:31:04 -0300

    Missão 1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import os
 
 cor_vermelho = "\033[31m"
 cor_verde = "\033[32m"
@@ -13,13 +14,34 @@ reset = "\033[0m"
 linha_anterior = "\033[F"
 cima_da_linha = "\033[A"
 
+condicao = True 
 
+while condicao: 
+    os.system('cls' if os.name == 'nt' else 'clear')
 
+    print("[SIMULADOR DE INVESTIMENTOS RECORRENTES]\n")
+    time.sleep(0.5)
+    print(f"\n{italico}Bem vindo, vamos simular também investimentos recorrentes!{reset}")
+    print(f"{italico}Neste exercício vamos usar somente LCIs, sem cálculo de IR dessa vez{reset}\n")
+    time.sleep(1.0)
 
+    print(f"\n{italico}Iniciando as simulações...{reset}")
+    time.sleep(1.5)
 
+    opcao = input(f"\nDigite {cor_azul}[novo]{reset} investimento, {cor_azul}[sair]{reset} ou aperte {cor_azul}[enter]{reset} para avançar em um mês: ").lower()
 
-    
+    if opcao == "sair":
+        print("\nObrigado por usar o simulador. Até logo!")
+        condicao = False
 
+    elif opcao == "novo":
+        print("\nIniciando cadastro de um novo investimento...")
+        time.sleep(1.0)
 
-   
+    elif opcao == "":
+        print("\nAvançando um mês...")
+        time.sleep(1.0)
 
+    else:
+        print("\nOpção inválida! Por favor, digite 'novo', 'sair' ou pressione 'enter' para continuar.")  
+        time.sleep(1.0)        
