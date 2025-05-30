b447bbb8de99f33e99a6dcd0dfd3500f09b8b57f
analuisavier <analuivieira@hotmail.com>
2025-05-29 22:49:24 -0300

    Inicializando projeto

---
+++ main.py
@@ -0,0 +1,25 @@
+import time
+
+cor_vermelho = "\033[31m"
+cor_verde = "\033[32m"
+cor_azul = "\033[34m"
+cor_amarelo = "\033[33m"
+cor_roxo = "\033[35m"
+cor_ciano = "\033[36m"
+cor_branco = "\033[37m"
+italico = "\033[3m"
+negativo = "\033[7m"
+reset = "\033[0m"
+linha_anterior = "\033[F"
+cima_da_linha = "\033[A"
+
+
+
+
+
+
+    
+
+
+   
+
